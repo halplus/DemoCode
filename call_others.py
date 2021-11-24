f54fd2ae67c794ccb31220678440299ce6b16a33
halplus <mehuangliang@gmail.com>
2021-11-24 17:17:26 -0500

    Add files via upload

---
+++ call_others.py
@@ -0,0 +1,5 @@
+import os
+
+if __name__ == "__main__":
+    os.system("python3 /home/feishao/sysrep_test_temp/write_home.py")
+    os.system("python3 /home/feishao/sysrep_test_temp/python_write_random_data.py")
